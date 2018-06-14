9f6bf84dbe3eebfe128af0f2a4e72bf8a8b6c595
crupest <crupest@outlook.com>
2018-06-14 21:16:44 +0800

    Temporary commit.

---
+++ CruUI/UI/Controls/TextBlock.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "UI/UIBase.h"
+#include "UI/Control.h"
+
+namespace cru
+{
+	namespace ui
+	{
+		namespace controls
+		{
+			class TextBlock : public Control
+			{
+
+			};
+		}
+	}
+}
